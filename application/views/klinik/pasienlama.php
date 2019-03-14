996461422b06614c976fdf4cf2d91de99a604426
Khoiriri <xjrling@gmail.com>
2019-03-14 23:43:49 +0700

    Update pasienlama.php

--- application/views/klinik/pasienlama.php
+++ application/views/klinik/pasienlama.php
@@ -34,18 +34,6 @@
         </div>
       </div>
   </div>
-  
-            <div id="content">
-               <div class="panel box-shadow-none content-header">
-                  <div class="panel-body">
-                    <div class="col-md-12">
-                        <h3 class="animated fadeInLeft">Data Tables</h3>
-                        <p class="animated fadeInDown">
-                          Table <span class="fa-angle-right fa"></span> Data Tables
-                        </p>
-                    </div>
-                  </div>
-              </div>
               <div class="col-md-12 top-20 padding-0">
                 <div class="col-md-12">
                   <div class="panel">
