1a517247e5c60aabc83e671edc997a66ab9d3603
Khoiriri <xjrling@gmail.com>
2019-03-14 23:39:40 +0700

    tblfix

--- application/views/klinik/content.php
+++ application/views/klinik/content.php
@@ -1,3 +1,33 @@
+                 <!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <meta name="description" content="Miminium Admin Template v.1">
+  <meta name="author" content="PandawaDev">
+  <meta name="keyword" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>K-PAN</title>
+
+  <!-- start: Css -->
+  <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+
+  <!-- plugins -->
+  <link rel="stylesheet" type="text/css" href="asset/css/plugins/font-awesome.min.css"/>
+  <link rel="stylesheet" type="text/css" href="asset/css/plugins/simple-line-icons.css"/>
+  <link rel="stylesheet" type="text/css" href="asset/css/plugins/animate.min.css"/>
+  <link rel="stylesheet" type="text/css" href="asset/css/plugins/icheck/skins/flat/aero.css"/>
+  <link href="asset/css/style.css" rel="stylesheet">
+  <!-- end: Css -->
+
+  <link rel="shortcut icon" href="asset/img/logomi.png">
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+    </head>
+
 <!-- start: Content -->
   <div id="content">
       <div class="panel box-shadow-none content-header">
@@ -10,33 +40,92 @@
           </div>
         </div>
       </div>
-      <div class="form-element">
-        <div class="col-md-12 padding-0 col-md-offset-2">
-          <div class="col-md-8">
-            <div class="panel form-element-padding">
-              <div class="panel-heading">
-               <h4>Basic Element</h4>
+   
+
+    <body id="mimin" class="dashboard form-signin-wrapper">
+
+    
+
+        <form class="form-signin">
+          <div class="panel periodic-login">
+              <span class="atomic-number"></span>
+              <div class="panel-body text-center">
+               
+                  <p class="atomic-mass">Tambah Data Pasien Baru</p>
+                  <p class="element-name">Klinik-Ambulu</p>
+
+                  <i class="icons icon-arrow-down"></i>
+                  <div class="form-group form-animate-text" style="margin-top:40px !important;">
+                    <input type="text" class="form-text" required>
+                    <span class="bar"></span>
+                    <label>Kode Pasien</label>
+                  </div>
+				  
+                  <div class="form-group form-animate-text" style="margin-top:40px !important;">
+                    <input type="text" class="form-text" required>
+                    <span class="bar"></span>
+                    <label>Nama Pasien</label>
+                  </div>
+				  
+                   <div class="form-group form-animate-text" style="margin-top:40px !important;">
+                    <input type="text" class="form-text" required>
+                    <span class="bar"></span>
+                    <label>Alamat Pasien</label>
+                  </div>
+				  
+				   <div class="form-group form-animate-text" style="margin-top:40px !important;">
+                    <input type="Text" class="form-text" required>
+                    <span class="bar"></span>
+                    <label>Umur Pasien</label>
+                  </div>
+				  
+				   <div class="form-group form-animate-text" style="margin-top:40px !important;">
+                    <input type="datetime" class="form-text" required>
+                    <span class="bar"></span>
+                    <label>Tanggal Pendaftaran</label>
+                  </div>
+				  
+				   <div class="form-group form-animate-text" style="margin-top:40px !important;">
+                    <input type="text" class="form-text" required>
+                    <span class="bar"></span>
+                    <label>Nama Penyakit</label>
+                  </div>
+				  
+                  <label class="pull-left">
+                  <input type="checkbox" class="icheck pull-left" name="checkbox1"/> &nbsp Yakin semua data yang diinputkan benar
+                  </label>
+                  <input type="submit" class="btn col-md-12" value="Tambah"/>
               </div>
-               <div class="panel-body" style="padding-bottom:30px;">
-                <div class="col-md-12">
-                  <div class="form-group"><label class="col-sm-2 control-label text-right">Kode Pasien</label>
-                    <div class="col-sm-10"><input type="text" class="form-control" placeholder="angka saja"></div>
-                  </div>
-                  <div class="form-group"><label class="col-sm-2 control-label text-right">Nama Pasien</label>
-                    <div class="col-sm-10"><input type="text" class="form-control" placeholder="huruf saja"></div>
-                  </div>
-                  <div class="form-group"><label class="col-sm-2 control-label text-right">Alamat</label>
-                    <div class="col-sm-10"><input type="text" class="form-control" placeholder="huruf saja"></div>
-                  </div>
-                  <div class="form-group"><label class="col-sm-2 control-label text-right">Date</label>
-                    <div class="col-sm-10"><input type="Date" class="form-control" ></div>
-                  </div>
-                   </div>
+                <div class="text-center" style="padding:5px;">
+                    <a href="login.html"></a>
                 </div>
-              </div>
-            </div>
           </div>
-        </div>
+        </form>
+
+      </div>
+
+      <!-- end: Content -->
+      <!-- start: Javascript -->
+      <script src="asset/js/jquery.min.js"></script>
+      <script src="asset/js/jquery.ui.min.js"></script>
+      <script src="asset/js/bootstrap.min.js"></script>
+
+      <script src="asset/js/plugins/moment.min.js"></script>
+      <script src="asset/js/plugins/icheck.min.js"></script>
+
+      <!-- custom -->
+      <script src="asset/js/main.js"></script>
+      <script type="text/javascript">
+       $(document).ready(function(){
+         $('input').iCheck({
+          checkboxClass: 'icheckbox_flat-aero',
+          radioClass: 'iradio_flat-aero'
+        });
+       });
+     </script>
+     <!-- end: Javascript -->
+   </body>
+   </html>
       </div>
   </div>
 <!-- end: content -->
--- application/views/klinik/pasienlama.php
+++ application/views/klinik/pasienlama.php
@@ -1,26 +1,203 @@
+<head>
+ <!-- start: Css -->
+  <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+
+  <!-- plugins -->
+  <link rel="stylesheet" type="text/css" href="asset/css/plugins/font-awesome.min.css"/>
+  <link rel="stylesheet" type="text/css" href="asset/css/plugins/datatables.bootstrap.min.css"/>
+  <link rel="stylesheet" type="text/css" href="asset/css/plugins/animate.min.css"/>
+  <link href="asset/css/style.css" rel="stylesheet">
+  <!-- end: Css -->
+</head>
+  <ul class="nav navbar-nav search-nav">
+                <li>
+                   <div class="search">
+                    <span class="fa fa-search icon-search" style="font-size:23px;"></span>
+                    <div class="form-group form-animate-text">
+                      <input type="text" class="form-text" required>
+                      <span class="bar"></span>
+                      <label class="label-search">Type anywhere to <b>Search</b> </label>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+
 <!-- start: Content -->
 <div id="content">
    <div class="panel box-shadow-none content-header">
       <div class="panel-body">
         <div class="col-md-12">
-            <h3 class="animated fadeInLeft">Data Tables</h3>
+            <h3 class="animated fadeInLeft">Data Pasien Lama</h3>
             <p class="animated fadeInDown">
-              Table <span class="fa-angle-right fa"></span> Data Tables
+              Table <span class="fa-angle-right fa"></span> Data Pasien Lama
             </p>
         </div>
       </div>
   </div>
-  <div class="col-md-12 top-20 padding-0">
-    <div class="col-md-12">
-      <div class="panel">
-        <div class="panel-heading"><h3>Data Tables</h3></div>
-        <div class="panel-body">
-          <div class="responsive-table">
-          <h5>hello worlds</h5>
-          </div>
-      </div>
-    </div>
-  </div>
-  </div>
-</div>
+  
+                    <div class="panel-body">
+                      <div class="responsive-table">
+                      <table id="datatables-example" class="table table-striped table-bordered" width="100%" cellspacing="0">
+                      <thead>
+                        <tr>
+                          <th>Kode Pasien</th>
+                          <th>Nama Pasien</th>
+                          <th>Alamat Pasien</th>
+                          <th>Umur Pasien</th>
+                          <th>Tanggal Masuk</th>
+                          <th>Nama Penyakit</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>p01</td>
+                          <td>Sumanto</td>
+                          <td>Bondowoso</td>
+                          <td>35 tahun</td>
+                          <td>2011/04/25</td>
+                          <td>Stroke</td>
+                        </tr>
+                        <tr>
+                          <td>p02</td>
+                          <td>Sumina</td>
+                          <td>Jember</td>
+                          <td>63 tahun</td>
+                          <td>2011/07/25</td>
+                          <td>Kencing Manis</td>
+                        </tr>
+                        <tr>
+                          <td>p03</td>
+                          <td>Adit</td>
+                          <td>Jember</td>
+                          <td>18 tahun</td>
+                          <td>2009/01/12</td>
+                          <td>Tipus</td>
+                        </tr>
+                        <tr>
+                          <td>p04</td>
+                          <td>Bunga</td>
+                          <td>Bondowoso</td>
+                          <td>22 tahun</td>
+                          <td>2012/03/29</td>
+                          <td>Demam Berdarah</td>
+                        </tr>
+                        <tr>
+                          <td>p05</td>
+                          <td>Airin</td>
+                          <td>Bondowoso</td>
+                          <td>19 tahun</td>
+                          <td>2008/11/28</td>
+                          <td>Tipus</td>
+                        </tr>
+                        <tr>
+                          <td>p06</td>
+                          <td>Ilham</td>
+                          <td>Jember</td>
+                          <td>61 tahun</td>
+                          <td>2012/12/02</td>
+                          <td>Kanker</td>
+                        </tr>
+                        <tr>
+                          <td>p07</td>
+                          <td>Sales Tuki</td>
+                          <td>Banyuwangi</td>
+                          <td>59 tahun</td>
+                          <td>2012/08/06</td>
+                          <td>Kanker</td>
+                        </tr>
+                        <tr>
+                          <td>p08</td>
+                          <td>Inai</td>
+                          <td>Jember</td>
+                          <td>55 tahun</td>
+                          <td>2010/10/14</td>
+                          <td>Tipus</td>
+                        </tr>
+                        <tr>
+                          <td>p09</td>
+                          <td>Muimin</td>
+                          <td>Jember</td>
+                          <td>39 tahun</td>
+                          <td>2009/09/15</td>
+                          <td>Demam</td>
+                        </tr>
+                        <tr>
+                          <td>P10</td>
+                          <td>Sofian</td>
+                          <td>Jember</td>
+                          <td>23 tahun</td>
+                          <td>2008/12/13</td>
+                          <td>Tipus</td>
+                        </tr>
+                        <tr>
+                          <td>p11</td>
+                          <td>Ofichan</td>
+                          <td>Situbondo</td>
+                          <td>30 tahun</td>
+                          <td>2008/12/19</td>
+                          <td>Kanker</td>
+                        </tr>
+                        <tr>
+                          <td>p12</td>
+                          <td>Leli</td>
+                          <td>Banyuwangi</td>
+                          <td>5 tahun</td>
+                          <td>2013/03/03</td>
+                          <td>Demam</td>
+                        </tr>
+                        <tr>
+                          <td>p13</td>
+                          <td>Rahmati</td>
+                          <td>Situbondo</td>
+                          <td>36 tahun</td>
+                          <td>2008/10/16</td>
+                          <td>Kencing Manis</td>
+                        </tr>
+                        <tr>
+                          <td>p14</td>
+                          <td>Selamet</td>
+                          <td>Jember</td>
+                          <td>70 tahun</td>
+                          <td>2012/12/18</td>
+                          <td>Stroke</td>
+                        </tr>
+                        <tr>
+                          <td>P15</td>
+                          <td>Sumiati</td>
+                          <td>Banyuwangi</td>
+                          <td>19 tahun</td>
+                          <td>2010/03/17</td>
+                          <td>Tipus</td>
+                        </tr>
+                      
+                      </tbody>
+                        </table>
+                      </div>
+                  </div>
+                </div>
+              </div>  
+              </div>
+            </div>
 <!-- end: content -->
+
+<!-- start: Javascript -->
+<script src="asset/js/jquery.min.js"></script>
+<script src="asset/js/jquery.ui.min.js"></script>
+<script src="asset/js/bootstrap.min.js"></script>
+
+
+
+<!-- plugins -->
+<script src="asset/js/plugins/moment.min.js"></script>
+<script src="asset/js/plugins/jquery.datatables.min.js"></script>
+<script src="asset/js/plugins/datatables.bootstrap.min.js"></script>
+<script src="asset/js/plugins/jquery.nicescroll.js"></script>
+
+
+<!-- custom -->
+<script src="asset/js/main.js"></script>
+<script type="text/javascript">
+  $(document).ready(function(){
+    $('#datatables-example').DataTable();
+  });
+</script>
